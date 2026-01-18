7e0d95c4de08ddb7d642d35e8537faa84acfbeb2
elidellman <elidellman@gmail.com>
2026-01-18 14:38:39 -0400

    Fixed gemini url

--- backend/Crossword/HandleCrossword.js
+++ backend/Crossword/HandleCrossword.js
@@ -1,5 +1,5 @@
-import {geminiForSynonyms} from "../Gemini/callGemini.js"
-import {geminiForClues} from "../Gemini/callGemini.js"
+import {geminiForSynonyms} from "../Gemini/CallGemini.js"
+import {geminiForClues} from "../Gemini/CallGemini.js"
 
 let cluesDoneLoading = false;
 
