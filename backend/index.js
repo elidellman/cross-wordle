bad33057d57e2ab04bd37324149200b5ed96baf9
elidellman <elidellman@gmail.com>
2026-01-26 18:52:47 -0400

    sa

--- backend/index.js
+++ backend/index.js
@@ -122,20 +122,21 @@ app.post("/api/is-valid-word", async (req, res) => {
 
     const result = await isValidWordle(req.body.text);
     console.log(result);
-    if (result) {
-        // if word is valid generate and store new words
-        if(req.body.answer === currentWordleAnswer){
-            await addSynonymsToList(req.body.answer).then((result2) => {
-            });
-        }else{
-            addSynonymsToList(req.body.answer).then((result2) => {
-            });
-        }
-        // input is word to generate synonyms/related words for
-
-    }
-
-    res.send(result);
+    res.send(false);
+    // if (result) {
+    //     // if word is valid generate and store new words
+    //     if(req.body.answer === currentWordleAnswer){
+    //         await addSynonymsToList(req.body.answer).then((result2) => {
+    //         });
+    //     }else{
+    //         addSynonymsToList(req.body.answer).then((result2) => {
+    //         });
+    //     }
+    //     // input is word to generate synonyms/related words for
+    //
+    // }
+    //
+    // res.send(result);
 })
 
 app.post("/api/get-clue", (req, res) => {
